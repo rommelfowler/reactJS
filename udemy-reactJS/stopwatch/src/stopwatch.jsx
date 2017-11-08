b79f673c1822fb2339f26976a5f83baab1871820
rommel santiago <rommelsantiago@rommels-Mac-Pro.local>
2017-11-07 19:59:38 -0800

    stopwatch

---
+++ udemy-reactJS/stopwatch/src/stopwatch.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import './stopwatch.css';
+
+class Stopwatch extends Component  {
+
+
+
+  render(){
+    return(
+      <div>
+        <div>
+          <h1>Stopwatch Component</h1>
+        </div>
+
+
+
+      </div>
+    )
+  }
+}
+
+export default Stopwatch;
