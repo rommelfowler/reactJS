3c307f60d6e501fe6865bafa9ea24c080e0869dd
Rommel Fowler <rommelfowler@Rommel-Fowler-Macbook-Pro.local>
2017-11-14 09:18:36 -0800

    dependencies

--- project-reactJS/youtubeweb-reactjs/src/App.js
+++ project-reactJS/youtubeweb-reactjs/src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        Test
       </div>
     );
   }
