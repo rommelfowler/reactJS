fa2499b6666892c2648af0139714e03b9c53adb2
rommel santiago <rommelsantiago@rommels-Mac-Pro.local>
2017-11-13 20:21:27 -0800

    music app update

--- udemy-reactJS/musicmaster/src/App.jsx
+++ udemy-reactJS/musicmaster/src/App.jsx
@@ -5,12 +5,22 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      query : "",
+      query : ''
 
     }
   }
   search(){
     console.log('this.state', this.state);
+    const BASE_URL=  'https://api.spotify.com/v1/search?';
+    const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+    console.log('FETCH_URL', FETCH_URL);
+    fetch(FETCH_URL, {
+      method: 'GET'
+    })
+    .then(response => {
+      const json = response.json();
+      console.log('json', json);
+    })
   }
 
   render() {
