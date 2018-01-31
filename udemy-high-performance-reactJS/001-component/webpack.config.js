4e7aeadf4637d12521f8be0b63a7eaf7b3db2153
Rommel Fowler <rommelfowler@Rommels-Mac-Pro.local>
2018-01-31 12:14:25 -0800

    updating webpack

--- udemy-high-performance-reactJS/001-component/webpack.config.js
+++
@@ -1,8 +0,0 @@
-// when webpack is loaded it looks for this method.
-module.exports = {
-  entry: './src/client.js', //starting file for client side app
-  output: {
-    path: "./public",
-    filename: 'index.js'
-  }
-}; // configure webpack, not es6 standard.
